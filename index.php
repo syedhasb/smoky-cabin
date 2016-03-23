d4bd055b85a3b3d00d83569645b4fe35b2d3bc44
alleydcruz <alley.dcruz@hotmail.com>
2016-03-23 16:28:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Smoky_Cabin
+ */
+
+get_header(); ?>
+
 <ul class="slides">
     <input type="radio" name="radio-btn" id="img-1" checked />
     <li class="slide-container">
@@ -74,22 +90,6 @@
       <label for="img-6" class="nav-dot" id="img-dot-6"></label>
     </li>
 </ul>
-
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Smoky_Cabin
- */
-
-get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
