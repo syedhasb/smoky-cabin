fb777a77027687450143e35062b6b04102a41338
ycpluss <yap.chs@live.com>
2016-03-23 17:26:32 -0400

    updated once again
    updated the comments

--- options.php
+++ options.php
@@ -11,39 +11,39 @@ function smokycabin_submenu() { /* This creates a new submenu in wordpress dashb
 	}
 	add_action( 'admin_menu', 'smokycabin_submenu' );
 
-function smokycabin_admin_menu(){ /* Add a new submenu in the wordpress dashboard and give it a name of "My Options Page */
+function smokycabin_admin_menu(){ /* This code should add a submenu to wordpress dashboard */
 	add_menu_page( 'My Options Page', 'My Options Page', 'manage_options', 'smoky_cabin_options_page', 'smoky_cabin_options_page', 'dashicons-hammer', 66 );
 	}
 	add_action( 'admin_menu', 'smokycabin_admin_menu' );
 	
-function smokycabin_setting(){ /* Registers the submenu in the wordpress dashboard */
+function smokycabin_setting(){ /* This should register submenu to dashboard */
 	register_setting('theme_options', 'options_settings');
 	add_settings_section( 'options_page',
 	 __( 'Your section description', 
 	 'smoky-cabin' ), 
 	 'options_page_callback', 
 	 'theme_options');
-	function options_page_callback() { /* Adds the below items in the options page form */
+	function options_page_callback() { /* Adds items below options page form */
 		echo __( 'A description and detail about the section.','smoky-cabin' );
 		}
-	add_settings_field( 'smokycabin_text', __('Enter the title for your options page: ', 'smoky-cabin'), 'smokycabin_text_render', 'theme_options', 'options_page');/* Registers a text box in the form of the Options Page */
-	add_settings_field( 'smokycabin_checkbox', __( 'Check your preference', 'smoky-cabin' ), 'smokycabin_checkbox_render', 'theme_options', 'options_page');/* Registers a checkbox in the form of the Options Page */
-	add_settings_field( 'smokycabin_textarea', __( 'Enter sponsors that you might have:', 'smoky-cabin' ), 'smokycabin_textarea_render', 'theme_options', 'options_page');/* Registers a text area in the form of the Options Page */
-	function smokycabin_text_render() {/* Create a text box in the form of the Options Page */
+	add_settings_field( 'smokycabin_text', __('Enter the title for your options page: ', 'smoky-cabin'), 'smokycabin_text_render', 'theme_options', 'options_page');/* Sign up a textbox in options page */
+	add_settings_field( 'smokycabin_checkbox', __( 'Check your preference', 'smoky-cabin' ), 'smokycabin_checkbox_render', 'theme_options', 'options_page');/* Sign up a checkbox in  Options Page */
+	add_settings_field( 'smokycabin_textarea', __( 'Enter sponsors that you might have:', 'smoky-cabin' ), 'smokycabin_textarea_render', 'theme_options', 'options_page');/* Sign up a text area in Options Page */
+	function smokycabin_text_render() {/* Create a text box in Options Page */
 		$options=get_option( 'options_settings' );
 		?>
 		<input type="text" name="options_settings[smokycabin_text]" value="<?php if (isset($options['smokycabin_text'])) 
 		echo $options['smokycabin_text']; ?>">
 		<?php
 	}
-	function smokycabin_checkbox_render() { /* Create a checkbox in the form of the Options Page */
+	function smokycabin_checkbox_render() { /* Create a checkbox in Options Page */
 		$options=get_option( 'options_settings' );
 		?>
 		<input type="checkbox"name="options_settings[smokycabin_checkbox]"
 		<?php if (isset($options['smokycabin_checkbox'])) checked( $options['smokycabin_checkbox'], 1); ?>value="1">
 		<?php
 	}
-	function smokycabin_textarea_render() { /* Create a text area in the form of the Options Page */
+	function smokycabin_textarea_render() { /* Create a text area in Options Page */
 		$options=get_option( 'options_settings' );
 		?>
 		<textarea cols="40"rows="5"name="options_settings[smokycabin_textarea]">
@@ -51,7 +51,7 @@ function smokycabin_setting(){ /* Registers the submenu in the wordpress dashboa
 		<?php
 		}
 }
-function smoky_cabin_options_page(){ /* Create the form in the Options Page so the user can input their own customizations to the page */
+function smoky_cabin_options_page(){ /* This allows users to input customizations to the page */
 ?>
 	<form action="options.php"method="post"><h2>Options Page</h2>
 	<?php
