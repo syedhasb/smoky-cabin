62ab76059522ec0db827801f4e5560319a63c7ca
ycpluss <yap.chs@live.com>
2016-03-29 17:47:50 -0400

    Update smokycabinwidget.php
    Created the back end form and echoes titles and descriptions,

--- smokycabinwidget.php
+++ smokycabinwidget.php
@@ -40,6 +40,7 @@ function register_my_post() {
 				)
 			)
 		);
+}
 
 //this section is from the example given in class. this activates the widget domain
 class smokycabin_widget extends WP_Widget {
@@ -59,20 +60,60 @@ class smokycabin_widget extends WP_Widget {
 		if ( ! empty( $title ) )
 			echo $args['before_title'] . $title . $args['after_title'];
 		
+// This echoes the post on Smoky cabin with image title and descriptions
+		echo "smokycabin ";
+			$args = array('post_type'=>'post');
+			$query = new WP_Query($args);
+			if ($query ->have_posts() ) {
+				echo '<ul class="smokycabin_widget">';
+				while($query->have_posts()) {
+					$query->the_post();
+					$smokycabin = '<li>' . $image;
+					$smokycabin .= '<a href="' . get_permalink() . '">';
+					$smokycabin .= get_the_title() . '</a>';
+					$smokycabin .= '<span>' . get_the_excerpt() . '';
+					$smokycabin .= '</span></li>';
+					echo $smokycabin;
+				}
+			echo '</ul>';
+			wp_reset_postdata();
+		}
+			}
+
+// This was taken from the example code given in class; this is the argument for the title of the form
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'smokycabin_widget_domain' );
+		}
+
+// This was from the example code given in class; it displays the form for the backend
 ?>
-<!-- This creates the widget admin form -->
 <p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'smokycabin'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
-		</p>
+	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); 	?></label>
+	<input class="smoky" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+
 <?php
 	}
-// this displays the titles for the posts
-// this is the backend form
 
-	
-//register the wordpress widget
-function SC_widget(){
-	register_widget('smokycabin_widget' );
-	add_action( 'widgets_init', 'smokycabin_widget' );
+
+//This was taken from an example code given in class; this is what saves and submits the widget
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}
+} 
+
+// This was taken from an example code given in class; this registers and load the widget
+function smokycabin_load_widget() {
+    register_widget( 'smokycabin_widget' );
+}
+
+add_action( 'widgets_init', 'smokycabin_load_widget' );
+
+
 ?>
