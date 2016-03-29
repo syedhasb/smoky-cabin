56da0675114bce1fbbdc0d515c899486c4626845
alleydcruz <alley.dcruz@hotmail.com>
2016-03-29 11:46:23 -0400

    Update template-map.php

--- template-map.php
+++ template-map.php
@@ -2,16 +2,16 @@
 /**
  * Template Name: Grid Map Page 
  *
- * @package Fluffy
+ * The Smoky Cabin
  */
 
 get_header(); ?>
 
-<!-- this is the grid ID -->
+<!-- Thid code allows the user to style the grid-->
 	<div id="grid" class="content-area">
 		<main id="main" class="site-main" role="main">
 		
-<!-- Below is the content area	-->		
+<!-- The code below is for the contet area-->		
 			<?php the_content (); ?> 
 			
 
@@ -21,7 +21,7 @@ get_header(); ?>
 		$my_query = new WP_Query ($args);
 	 if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post(); ?> 
 
-<!-- this is where you call the grid -->	
+<!-- This code allows the user to call the grid -->	
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
 		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 		<div id="gridstyle"> 
@@ -33,8 +33,8 @@ get_header(); ?>
 	<?php endwhile; endif; ?>
 
 <!-- the navigation to previous and next in the post category --> 			
-	<div class="nav-previous"><?php next_posts_link(__( '&larr; GO OLDER', 'yapchengsiew' ) ); ?></div>
-<div class="nav-next"><?php previous_posts_link( __( 'GO NEWER &rarr;', 'yapchengsiew' )  ); ?></div>
+	<div class="nav-previous"><?php next_posts_link(__( '&larr; GO OLDER', 'smokycabin' ) ); ?></div>
+<div class="nav-next"><?php previous_posts_link( __( 'GO NEWER &rarr;', 'smokycabin' )  ); ?></div>
 		
 
 		</main>
